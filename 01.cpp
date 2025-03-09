dd8a112015374b1111c57bdb7534757f87189b8d
nguyenntkn0538 <channtkn@gmail.com>
2025-03-09 18:52:14 +0700

    Add files via upload

---
+++ 01.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+float x;
+printf(" Nhap so:");
+scanf("%f",&x);
+float s=pow(x,2),q=pow(x,3),k=pow(x,4);
+printf("%f %f %f",s,q,k);
+return 0;
+
+}
